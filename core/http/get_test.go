c24ffa687fec12f4613d080a872da3d3fc60af8a
chest <tinychest@foxmail.com>
2021-09-22 13:43:19 +0800

    feat：使用 HTTP 请求模块发起请求的简单用例

--- core/http/get_test.go
+++ core/http/get_test.go
@@ -2,40 +2,12 @@ package http
 
 import (
 	"fmt"
-	"net/http"
-	"net/url"
+	"go-learn/util/req"
 	"testing"
 )
 
-const URL = "http://127.0.0.1:8080"
-
-type XxxResult struct {
-	Xxx string
-}
-
-// 详见 http 包的注释，有给出发起各种请求的例子
-// 点：The client must close the response body when finished with it（client 单纯表示客户端的意思）
 func TestGet(t *testing.T) {
-	// 1、准备参数
-	params := url.Values{
-		"name": []string{"xiaoming"},
-		"age":  []string{"11"},
-	}
-	var (
-		url  = fmt.Sprintf("%s?%s", URL, params.Encode())
-		resp *http.Response
-		err  error
-	)
-
-	// 2、发起请求
-	if resp, err = http.Get(url); err != nil {
-		t.Fatal(err)
-	}
-
-	// 3、处理结果
-	var result = new(XxxResult)
-	if err = ParseJsonResponse(resp, result); err != nil {
-		t.Fatal(err)
-	}
-	_ = resp.Body.Close()
+	var result map[string]interface{}
+	err := req.GetJson("http://127.0.0.1:8888/v1/red_pack/test", nil, result)
+	fmt.Println(err)
 }
