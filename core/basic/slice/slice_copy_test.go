f9e467ca4a69d01262e93eada7facb6f0af22f67
chest <tinychest@foxmail.com>
2021-11-04 09:18:05 +0800

    style：命名风格

--- core/basic/slice/slice_copy_test.go
+++ core/basic/slice/slice_copy_test.go
@@ -5,17 +5,17 @@ import (
 )
 
 func TestCopy(t *testing.T) {
-	sourceIntSlice := []int{1, 2, 3}
+	source := []int{1, 2, 3}
 
-	var targetIntSlice1 []int
-	targetIntSlice2 := make([]int, 0)
-	targetIntSlice3 := make([]int, 0, 3)
-	targetIntSlice4 := make([]int, 3)
+	var target1 []int
+	target2 := make([]int, 0)
+	target3 := make([]int, 0, 3)
+	target4 := make([]int, 3)
 
-	println(copy(targetIntSlice1, sourceIntSlice)) // 0
-	println(copy(targetIntSlice2, sourceIntSlice)) // 0
-	println(copy(targetIntSlice3, sourceIntSlice)) // 0
-	println(copy(targetIntSlice4, sourceIntSlice)) // 3
+	println(copy(target1, source)) // 0
+	println(copy(target2, source)) // 0
+	println(copy(target3, source)) // 0
+	println(copy(target4, source)) // 3
 
 	// 结论：只有有空间的切片或数组 copy 才能够复制
 }
