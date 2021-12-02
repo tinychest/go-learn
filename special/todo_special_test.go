ee3e24536fa68c0ad95852733c064dfef23fa2ae
chest <tinychest@foxmail.com>
2021-12-02 16:49:09 +0800

    feat：todo special case

---
+++ special/todo_special_test.go
@@ -0,0 +1,31 @@
+package special
+
+import (
+	"fmt"
+	"testing"
+	"unsafe"
+)
+
+// https://philpearl.github.io/post/anathema/
+func TestRun(t *testing.T) {
+	var limited int
+	var out int
+	doThing(out, func(out interface{}) {
+		if limited == 0 {
+			fmt.Printf("limited is zero. %d\n", limited) // Prints 42
+		}
+		limited++
+	})
+}
+
+type eface struct {
+	rtype unsafe.Pointer
+	data  unsafe.Pointer
+}
+
+//go:noinline
+func doThing(out interface{}, f func(out interface{})) {
+	p := (*eface)(unsafe.Pointer(&out)).data
+	*(*int)(p) = 42
+	f(out)
+}
