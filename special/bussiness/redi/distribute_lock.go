487b2c45186969690ce8c9a8c854f03516c9270f
chest <tinychest@foxmail.com>
2022-01-05 10:17:40 +0800

    style：redis sub and pub

--- special/bussiness/redi/distribute_lock.go
+++ special/bussiness/redi/distribute_lock.go
@@ -4,46 +4,7 @@ import (
 	"fmt"
 	"github.com/gomodule/redigo/redis"
 	"sync"
-	"time"
 )
-
-/*
-引入依赖 github.com/gomodule/redigo v1.8.5
-*/
-
-var pool *redis.Pool
-
-func init() {
-	pool = &redis.Pool{
-		MaxIdle:     3,
-		IdleTimeout: 240 * time.Second,
-		// Dial or DialContext must be set. When both are set, DialContext takes precedence over Dial.
-		Dial: func() (redis.Conn, error) {
-			c, err := redis.Dial("tcp", "localhost:6379")
-			if err != nil {
-				return nil, err
-			}
-			// 没有密码，就不要进行这一步了
-			// if _, err = c.Do("AUTH"); err != nil {
-			// 	_ = c.Close()
-			// 	return nil, err
-			// }
-			if _, err = c.Do("SELECT", 0); err != nil {
-				_ = c.Close()
-				return nil, err
-			}
-			return c, nil
-		},
-		// Other pool configuration not shown in this example.
-		TestOnBorrow: func(c redis.Conn, t time.Time) error {
-			if time.Since(t) < time.Minute {
-				return nil
-			}
-			_, err := c.Do("PING")
-			return err
-		},
-	}
-}
 
 func incr() {
 	client := pool.Get()
@@ -79,7 +40,7 @@ func incr() {
 	}
 }
 
-func main() {
+func main1() {
 	var wg sync.WaitGroup
 	for i := 0; i < 10; i++ {
 		wg.Add(1)
---
+++ special/bussiness/redi/pub_sub.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gomodule/redigo/redis"
+)
+
+func main() {
+	const chanName = "ccc"
+
+	conn := pool.Get()
+
+	psc := redis.PubSubConn{Conn: conn}
+
+	if err := psc.Subscribe(redis.Args{}.AddFlat(chanName)...); err != nil {
+		panic(err)
+	}
+	done := make(chan error, 1)
+	// start a new goroutine to receive message
+	go func() {
+		defer psc.Close()
+		for {
+			switch msg := psc.Receive().(type) {
+			// 发生错误
+			case error:
+				fmt.Println("error", msg)
+				done <- fmt.Errorf("redis pubsub receive err: %v", msg)
+				return
+			// publish（普通的事件发布）
+			case redis.Message:
+				fmt.Println("message", msg.Channel, string(msg.Data))
+			// subscribe, unsubscribe, psubscribe or punsubscribe（订阅新增或减少 - 会检测到自己订阅的事件）
+			case redis.Subscription:
+				fmt.Println("subscription", msg.Channel, msg.Count, msg.Kind)
+				// channel do not have any subscribed channel
+				if msg.Count == 0 {
+					done <- nil
+					return
+				}
+			default:
+				panic(fmt.Sprintf("unknown msg type: %s", msg))
+			}
+		}
+	}()
+
+	// GG 不支持这个操作：unexpected response line (possible server error or unsupported concurrent read by application)
+	// sum, err := redis.Int(conn.Do("PUBSUB NUMPAT"))
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("PUBSUM NUMPAT:", sum)
+
+	<-done
+	if err := psc.Unsubscribe(chanName); err != nil {
+		panic(err)
+	}
+	// ctx, cancel := context.WithTimeout(context.Background(), 10 * time.Second)
+	// defer cancel()
+	//
+	// // health check
+	// tick := time.NewTicker(time.Minute)
+	// defer tick.Stop()
+	// for {
+	// 	select {
+	// 	case <-ctx.Done():
+	// 		if err := psc.Unsubscribe(); err != nil {
+	// 			panic(fmt.Errorf("redis pubsub unsubscribe err: %v", err))
+	// 		}
+	// 	case err := <-done:
+	// 		return err
+	// 	case <-tick.C:
+	// 		if err := psc.Ping(""); err != nil {
+	// 			panic(err)
+	// 		}
+	// 	}
+	// }
+
+}
