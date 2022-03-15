fa6ef82fc91b590d6f64d2282b478ca97310cc99
chest <tinychest@foxmail.com>
2022-03-15 10:12:07 +0800

    style

--- special/bussiness/pool/ants_test.go
+++ special/bussiness/pool/ants_test.go
@@ -11,18 +11,17 @@ import (
 // ants goroutine pool 中：【pool】【worker】 的设计、自旋锁的使用、Option 的自定义初始化策略的写法、panicHandler 的自定义
 // 源码非常有参考意义，但是和实际业务中的思考有一些出入，所以没有借鉴到真正想借鉴的，但是业务中的设计确实也没什么刺挑了
 func TestAntsPool(t *testing.T) {
-	var (
-		taskGetter = func(index int) func() {
-			return func() { println(index) }
-		}
-		pool, _ = ants.NewPool(100000, ants.WithPreAlloc(true))
-		err     error
-	)
+	taskGetter := func(index int) func() {
+		return func() { t.Log(index) }
+	}
+
+	pool, _ := ants.NewPool(100000, ants.WithPreAlloc(true))
+	defer pool.Release()
 
 	// 提交 100 个任务
 	for index := 0; index < 100; index++ {
-		if err = pool.Submit(taskGetter(index)); err != nil {
-			println(err)
+		if err := pool.Submit(taskGetter(index)); err != nil {
+			t.Fatal(err)
 			return
 		}
 	}
