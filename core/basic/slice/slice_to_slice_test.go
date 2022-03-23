b6723bcf4a142e021e01072971cf6d52c6a915bf
chest <tinychest@foxmail.com>
2022-03-23 18:23:29 +0800

    feat：完善子切片的概念说明和复用切片的样例

--- core/basic/slice/slice_to_slice_test.go
+++ core/basic/slice/slice_to_slice_test.go
@@ -5,8 +5,18 @@ import (
 	"testing"
 )
 
+// 从一个切片划分出一个子切片，从而复用切片的内存空间地址（实际业务开发种，这里也是最容易出现内存泄漏问题）
+// 语法1：slice[addr:len:cap]
+// - addr：起始的元素下标（默认 0，可以省略）
+// - len：从起始元素开始，划分的长度（默认当前切片的长度，不能省略）
+// - cap：从起始元素开始，划分的容量（默认当前切片的容量，不能省略）
+//
+// 语法2：slice[addr:len]
+// - addr：可以省略
+// - len：可以省略
+
 // 切片示例的切片声明不能超过父切片的上限，但是增加元素自动增长可以的
-func TestQuota1(t *testing.T) {
+func TestQuota(t *testing.T) {
 	slice := []int{1, 2, 3}
 
 	ref := slice[:1:2]
@@ -16,6 +26,22 @@ func TestQuota1(t *testing.T) {
 	ref = append(ref, 4)
 	util.PrintSlice(ref)
 	util.PrintSlice(slice)
+}
+
+// 可以通过只操作下标，来达到复用一个切片（复用内存地址）的目的，拿实际开发经验来说，非常容易出错
+// 所以，切片还是应该通过 append 来操作，那如何完整复用一个切片呢？
+func TestQuotaReuse(t *testing.T) {
+	s := make([]int, 0, 4)
+
+	s = s[0:0:cap(s)] // 三元 不省略写法
+	util.PrintSlice(s)
+	s = s[:0:cap(s)]  // 三元 省略写法
+	util.PrintSlice(s)
+
+	s = s[0:0]        // 二元不省略写法
+	util.PrintSlice(s)
+	s = s[:0]         // 二元省略写法
+	util.PrintSlice(s)
 }
 
 // 其实还是 append 和 切片的原理规则，当只有当 append 元素后，元素的数量大于 capacity，append 才会重新申请一个内存空间
@@ -42,8 +68,10 @@ func TestQuota3(t *testing.T) {
 	s := make([]string, 0, 4)
 	s = append(s, "1")
 	util.PrintSlice(s)
+
 	s = s[:0]
 	util.PrintSlice(s)
+
 	s = s[:1]
 	util.PrintSlice(s) // 从这里的结果可以了解到，确实是复用空间，原来位置的值都没有变
 
