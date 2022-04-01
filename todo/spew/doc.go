1cefe3b19c7e69b36845ab861419f14be15a3e6b
chest <tinychest@foxmail.com>
2022-04-01 11:43:16 +0800

    style：new third print pack rec

---
+++ todo/spew/doc.go
@@ -0,0 +1,6 @@
+package spew
+
+// 一个方便调试的结构体打印三方包
+// go get -u github.com/davecgh/go-spew/spew
+//
+// 类似的三方库还有 github.com/gookit/goutil/dump
--- todo/spew/spew_print.go
+++
@@ -1,7 +0,0 @@
-package spew
-
-/*
-一个方便调试的结构体打印三方包
-
-go get -u github.com/davecgh/go-spew/spew
-*/
