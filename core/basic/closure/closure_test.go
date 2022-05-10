d6b76c7c38efb9d22cac68a82b78cb4af4397d03
chest <tinychest@foxmail.com>
2022-05-10 10:27:28 +0800

    feat：完善闭包案例的结构

--- core/basic/closure/closure_test.go
+++ core/basic/closure/closure_test.go
@@ -3,9 +3,35 @@ package closure
 import "testing"
 
 func TestClosure(t *testing.T) {
+	outModifyTest(t)
+	inModifyTest(t)
+	inPassModifyTest(t)
+}
+
+func outModifyTest(t *testing.T) {
 	n := 0
 	f := func() { t.Log(n) }
 	n++
 
+	f()
+}
+
+func inModifyTest(t *testing.T) {
+	n := 0
+	f := func() { n++ }
+
+	f()
+	t.Log(n)
+}
+
+func inPassModifyTest(t *testing.T) {
+	n := 0
+	f := func() { n++ }
+
+	whatever(f)
+	t.Log(n)
+}
+
+func whatever(f func()) {
 	f()
 }
