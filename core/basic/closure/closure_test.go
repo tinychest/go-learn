99c8d88b831227421b69aaf88129cea0e572b333
chest <tinychest@foxmail.com>
2022-05-25 09:43:16 +0800

    style：closure 案例整理

--- core/basic/closure/closure_test.go
+++ core/basic/closure/closure_test.go
@@ -2,6 +2,11 @@ package closure
 
 import "testing"
 
+// 闭包函数是什么，闭包函数就是有着变量捕获列表的函数
+// - 闭包很容易造成引用的变量发生逃逸
+//
+// 下面的若干的测试样例都是为了说明闭包函数对引用变量，引用的是其地址，所以能够改变其值。
+
 func TestClosure(t *testing.T) {
 	outModifyTest(t)
 	inModifyTest(t)
--- core/basic/closure/for_defer_func_test.go
+++ core/basic/closure/for_defer_func_test.go
@@ -4,26 +4,30 @@ import (
 	"testing"
 )
 
-var s = []string{"a", "b", "c"}
+// 以一个最简单的闭包问题为背景，循序渐进，演示解决步骤，同时阐明闭包问题的核心
+
+var x = []int{1, 2, 3}
 
 func TestDeferFunc(t *testing.T) {
-	mistake(t)
-	tryFix(t)
-	fix1(t)
-	fix2(t)
+	// mistake(t)
+	// tryFix(t)
+	// fix1(t)
+	// fix2(t)
+	// extend3(t)
 }
 
+// 1.没有按照预期打印
 func mistake(t *testing.T) {
-	for _, v := range s {
+	for _, v := range x {
 		defer func() {
 			t.Log(v)
 		}()
 	}
 }
 
-// 失败原因：并未改变函数内对 for 局部变量的闭包引用
+// 2.仍没有按照预期打印
 func tryFix(t *testing.T) {
-	for _, v := range s {
+	for _, v := range x {
 		defer func() {
 			i := v
 			t.Log(i)
@@ -31,9 +35,9 @@ func tryFix(t *testing.T) {
 	}
 }
 
-// 局部函数对未定义参数 - 外部变量的直接引用称作闭包
+// 3.正确。解决关键在于每个函数绑定的闭包变量都是一个新的
 func fix1(t *testing.T) {
-	for _, v := range s {
+	for _, v := range x {
 		v := v
 		defer func() {
 			t.Log(v)
@@ -41,13 +45,35 @@ func fix1(t *testing.T) {
 	}
 }
 
-// 为什么能解决：不要被冗杂的语法迷惑了双眼，循环体只要看成一个函数的调用即可
-// 方法传参是对变量值的引用，闭包是对变量的引用
+// 4.闭包的关键一个是局部函数，一个是函数内部对上下文变量的引用；很明显下面没有定义局部函数
 func fix2(t *testing.T) {
-	for _, v := range s {
-		// defer func(v string) {
-		// 	t.Log(v)
-		// }(v)
+	for _, v := range x {
 		defer t.Log(v)
+	}
+}
+
+// 5.拓展说明一下，for 循环背景下，闭包问题的一个要素，就是延时
+func extend3(t *testing.T) {
+	// 没有问题
+	for _, v := range x {
+		func() {
+			t.Log(v)
+		}()
+	}
+	// 延时1
+	for _, v := range x {
+		defer func() {
+			t.Log(v)
+		}()
+	}
+	// 延时2
+	fs := make([]func(), 0, len(x))
+	for _, v := range x {
+		fs = append(fs, func() {
+			t.Log(v)
+		})
+	}
+	for _, f := range fs {
+		f()
 	}
 }
