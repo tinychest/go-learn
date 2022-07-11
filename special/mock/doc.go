e9f007dc35fe13ca0fb39abb0dfd47e9fde28c81
chest <tinychest@foxmail.com>
2022-07-11 17:15:18 +0800

    style

--- special/mock/doc.go
+++ special/mock/doc.go
@@ -1,8 +1,7 @@
-/*
-1.创建 db.go
-2."mock" Open In Terminal
-3.mockgen -source=db.go -destination=db_mock.go -package=mock → 回生成 db_mock.go
-4.创建 db_test.go
-5.执行测试 go test . -cover -v
-*/
 package mock
+
+// 1.创建 db.go → 编写要测试方法
+// 2."mock" Open In Terminal
+// 3. 生成包含打桩的相关辅助方法的文件 db_mock.go：mockgen -source=db.go -destination=db_mock.go -package=mock
+// 4.创建 db_test.go → 编写测试用例
+// 5.执行测试：go test . -cover -v
