18475d53c5c8601d0149c7e447801b39f72f84fa
chest <tinychest@foxmail.com>
2022-07-18 09:24:38 +0800

    refactor：printf 调整排版、补充样例

--- core/basic/printf_test.go
+++ core/basic/printf_test.go
@@ -2,17 +2,65 @@ package basic
 
 import (
 	"fmt"
+	"math"
 	"testing"
 )
+
+// 详见 fmt/doc.go
+//
+// 【占位符】
+// %p：地址
+// %T：类型
+// %v：对应类型的默认格式
+// %s：string、[]byte、error
+// %q：为所有的值添加引号
+// %t：布尔类型
+// ---
+// %b：二进制（binary）
+// %o：八进制（octal）
+// %d：十进制（decimal）
+// %x、%X：十六进制（hex）
+// ---
+// %U = U+%04X：Unicode 格式
+// ---
+// %e、%E：科学计数法
+// %f：浮点数
+// %q：单引号围绕的字符字面量
+// ---
+//
+// 【补充符】
+// % 和 紧跟着的类型修饰符，中间是可以添加一些补充符的
+// [-]
+// - 从右边填充
+// [+]
+// - 结构体 添加字段名
+// - 数值 正负号
+// - 字符串 ASCII 编码的字符
+// [#]
+// - 处理结构体 会添加完整包路径的类型名、会添加字段名
+// - 八进制 前导 0
+// - 十六进制 前导 0x
+// - %#p 十六进制的内存地址去掉前导 0x
+// [空格、0]
+// - 打印字符串或切片时，在字节之间用指定字符隔开
+// [数字]
+// - 填充多少个
+
+// 【builtin 包下有个 println】
+// - 仅支持最基础的类型（时间类型都不支持）
+// - 如非打印简单的 string 类型值，并不推荐使用
+
+// 【fmt.Printf 的行为决策优先级】
+// - 实现了 Error() string：
+//     打印 Error 方法的返回值
+// - 实现了 String() string：
+//     打印 String 方法的返回值
+// - 按照默认行为打印
 
 type Account struct {
 	username string
 	password string
 }
-
-// func (p *Account) Error() string {
-// 	return "error..."
-// }
 
 func (p *Account) String() string {
 	// 注意，不能在这里调用 fmt.Sprintf 方法 - 死递归
@@ -20,90 +68,41 @@ func (p *Account) String() string {
 	return "string..."
 }
 
-/*
-详见 fmt/doc.go
-
-《占位符》
- %p：地址
- %T：类型
- %v：对应类型的默认格式
- %s：string、[]byte、error
- %q：为所有的值添加引号
- %t：布尔类型
-
- %b：二进制（binary）
- %o：八进制（octal）
- %d：十进制（decimal）
- %x、%X：十六进制（hex）
-
- %U = U+%04X：Unicode 格式
-
- %e、%E：科学计数法
- %f：浮点数
- %q：单引号围绕的字符字面量
-
-《补充符》
- [start:%]
- [-]
- - 从右边填充
- [+]
- - 结构体 添加字段名
- - 数值 正负号
- - 字符串 ASCII 编码的字符
- [#]
- - 结构体 会添加完整包路径的类型名、会添加字段名
- - 八进制 前导 0
- - 十六进制 前导 0x
- - %#p 十六进制的内存地址去掉前导 0x
- [空格、0]
- - 打印字符串或切片时，在字节之间用指定字符隔开
- [数字]
- - 填充多少个
- [end:类型标识]
-*/
 func TestPrintf(t *testing.T) {
-	// a := &Account{username: "xiaoming", password: "123"}
+	classicCaseTest(t)
 
-	// vsFormatTest(t)
-	// qFormatTest(t)
+	// vsqFormatTest(t)
+	// vsqStructFormatTest(t)
+	// addSymbolTest(t)
+}
 
-	// +：{username:小明 password:123}
-	// t.Logf("%+v\n", a)
+func classicCaseTest(t *testing.T) {
+	// 不足两位在左边补 0 保证最少两位
+	fmt.Printf("%02d\n", 1)
+	fmt.Printf("%02d\n", 10)
+
+	// 右对齐的数组打印
+	maxWidth := 1 + int(math.Log10(float64(10000)))
+	for _, v := range []int{10, 100, 10000} {
+		fmt.Printf("%*d\n", maxWidth, v)
+	}
+
+	// 不足 4 位在右边补 空格 保证最少 4 位
+	fmt.Printf("%-4d%s\n", 1, "END")
+
+	// 浮点数：主要用于限制小数位数，默认策略是四舍五入
+	fmt.Printf("%.4f\n", 10.1)
+}
+
+func addSymbolTest(t *testing.T) {
+	a := &Account{username: "xiaoming", password: "123"}
+	// +：Error 实现 → Stringer 实现
+	fmt.Printf("%+v\n", a)
 	// #：main.User{username:"小明", password:"123"}
-	// t.Logf("%#v\n", a)
-
-	// builtInPrintTest(t)
+	fmt.Printf("%#v\n", a)
 }
 
-// builtin 包下有个 println，仅支持基础类型（时间类型都不支持） - 并不推荐使用
-func builtInPrintlnTest(t *testing.T) {
-	// println([]byte("123"))                   // [3/3]0xc000045f4d
-	// fmt.Println([]byte("123"))               // [49 50 51]
-	// fmt.Printf("%s\n", []byte("123")) // 123
-	// fmt.Printf("%v\n", []byte("123")) // [49 50 51]
-}
-
-func vsFormatTest(t *testing.T) {
-	var a = Account{"小明", "123"}
-
-	// case 实现了 Error() string：
-	//     打印 Error 方法的返回值
-	// case 实现了 String() string：
-	//     打印 String 方法的返回值
-	// default：
-	//     &{小明 123}
-	fmt.Println(&a)
-	fmt.Printf("%v\n", &a)
-	fmt.Printf("%s\n", &a)
-
-	// default：
-	//     {小明 123}
-	fmt.Println(a)
-	fmt.Printf("%v\n", a)
-	fmt.Printf("%s\n", a)
-}
-
-func qFormatTest(t *testing.T) {
+func vsqFormatTest(t *testing.T) {
 	theM := map[string]interface{}{
 		"a": 1,
 		"b": "2",
@@ -114,12 +113,18 @@ func qFormatTest(t *testing.T) {
 	fmt.Printf("%q\n", theM) // 引号
 }
 
-// 动态模板
-func temTemTest(t *testing.T) {
-	// Printf 格式模板中的特殊符号的转译字符是 %，如希望打印 % 字符，就得写成 %%
-	// template := fmt.Sprintf("%%-%ds address:%%p length:%%v capacity:%%v\n", len(slice.([]int)))
-	// println("template: ", template)
-	// fmt.Printf(template, fmt.Sprint(slice), slice, len(value), cap(value))
+func vsqStructFormatTest(t *testing.T) {
+	var a = Account{"小明", "123"}
+
+	// fmt.Println(&a)
+	// fmt.Printf("%v\n", &a)
+	// fmt.Printf("%s\n", &a)
+	// fmt.Printf("%q\n", &a)
+
+	fmt.Println(a)
+	fmt.Printf("%v\n", a)
+	fmt.Printf("%s\n", a)
+	fmt.Printf("%q\n", a)
 }
 
 // Sscanf 设计的非常见简单，要求非常严格的参数规则，否则：input does not match format
