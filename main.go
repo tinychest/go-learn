06391752444e040e55963cc6c24ecf4a8678e24d
chest <tinychest@foxmail.com>
2022-08-15 09:29:22 +0800

    style

--- main.go
+++ main.go
@@ -9,12 +9,26 @@ import (
 )
 
 func main() {
+	// 获取当前项目的执行路径
 	fmt.Println(getAppPath())
 }
 
+// 在 goland console 中打印出来的相关 exe 路径会被标注为红色
 func getAppPath() string {
-	f, _ := exec.LookPath(os.Args[0])
-	p, _ := filepath.Abs(f)
-	path := p[:strings.LastIndex(p, string(os.PathSeparator))]
-	return path
+	path := os.Args[0]
+	println(path)
+
+	path, err := exec.LookPath(path)
+	println(path)
+	if err != nil {
+		panic(err)
+	}
+
+	path, err = filepath.Abs(path)
+	println(path)
+	if err != nil {
+		panic(err)
+	}
+
+	return path[:strings.LastIndex(path, string(os.PathSeparator))]
 }
